41f643db1ec3661615d4de5277d38304d90879bf
xxy-mm <xxy.moming@gmail.com>
2024-02-20 10:39:11 +0800

    feat: add account modules
    1. account list
    2. account edit

--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express'
 import {
+  createAccount,
+  findAccountById,
+  listAccount,
+  updateAccount,
+} from '../db/models/AccountModel'
+import {
   checkIsBefore,
   checkIsDate,
   checkIsNumber,
@@ -13,43 +19,100 @@ import { setFormToken } from '../utils/setFormToken'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.render('account-list')
+router.get('/', async (req, res) => {
+  const list = await listAccount({
+    _id: req.session!.user._id,
+    skip: 0,
+    limit: 10,
+  })
+
+  res.render('accountList', { list })
 })
 
 router.get('/new', (req, res) => {
   const form = new PageFormData({ createdAt: toFormDate(new Date()) }, {})
   setFormToken(req, form)
-  res.render('createAccount', form)
+  res.render('createOrUpdateAccount', { form, isNew: true })
 })
 
 router.post(
-  '/',
+  '/new',
   checkSessionToken('token'),
   checkNotEmpty('amount'),
   checkNotEmpty('details'),
-  checkNotEmpty('createdAt'),
+  checkNotEmpty('date'),
   checkIsNumber('amount'),
   checkLength('details', { min: 1, max: 200 }),
-  checkIsDate('createdAt'),
-  checkIsBefore('createdAt'),
+  checkIsDate('date'),
+  checkIsBefore('date'),
   (req, res) => {
     const form: PageFormData = req.session!.form
     if (form.hasError) {
       setFormToken(req, form)
-      res.render('createAccount', form)
+      res.render('createOrUpdateAccount', form)
       return
     }
-    res.json(req.body)
+    // invalid form token from session
+    req.session!.token = null
+    const { date, amount, details } = req.body
+    const user = req.session!.user._id as string
+    createAccount({
+      user,
+      date,
+      amount,
+      details,
+    })
+      .then((result) => {
+        return res.redirect('/accounts')
+      })
+      .catch((err) => {
+        return res.render('error', err)
+      })
   },
 )
 
-router.put('/:id', (req, res) => {
-  res.send('update account')
+router.get('/edit/:id', async (req, res) => {
+  const account = await findAccountById(req.params.id)
+  if (account == null) {
+    return res.render('error', { message: '404 not found' })
+  }
+
+  const form = new PageFormData()
+  form.values = {
+    ...account,
+    _id: account._id.toString(),
+    user: account.user.toString(),
+  }
+  setFormToken(req, form)
+  res.render('createOrUpdateAccount', { form, isNew: false })
 })
 
 router.delete('/:id', (req, res) => {
   res.send('delete account')
 })
+
+router.post(
+  '/edit',
+  checkSessionToken('token'),
+  checkNotEmpty('_id'),
+  checkIsNumber('amount'),
+  checkLength('details', { min: 10, max: 200 }),
+  async (req, res) => {
+    const form = req.session!.form
+    if (form.hasError) {
+      setFormToken(req, form)
+      res.render('createOrUpdateAccount', { form, isNew: false })
+      return
+    }
+    const updated = await updateAccount({
+      _id: req.body._id,
+      amount: req.body.amount,
+      details: req.body.details,
+    }).then((result) => {
+      req.session!.token = null
+      res.redirect('/accounts')
+    })
+  },
+)
 
 export default router
